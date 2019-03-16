694f503fc0cee6ea63694707dd8a0bbfc7dfc3aa
harrychuang <vrjgik5@gmail.com>
2019-03-16 14:34:38 -0700

    added a constructor for Entry Class

--- readFile.js
+++ readFile.js
@@ -3,3 +3,14 @@ function readJsonFile() {
         console.log(json); // this will show the info it in firebug console
     });
 }
+
+function Entry (obj) {
+    this.NAME = obj.NAME;
+    this.ADDRESS = obj.ADDRESS;
+    this.LATITUDE = obj.LATITUDE ;
+    this.LONGITUDE = obj.LONGITUDE ;
+    this.AMENITY_TYPE = obj.AMENITY_TYPE ;
+    this.PHONE_NUMBER = obj.PHONE_NUMBER ;
+    this.ENVIRONMENT = obj.ENVIRONMENT ;
+    this.COUNTER = obj.COUNTER;
+}
