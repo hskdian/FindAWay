fd8500f0fa428938f36266308479fb978b2d640b
harrychuang <vrjgik5@gmail.com>
2019-03-16 17:49:53 -0700

    the markers are on the map!!!!!!!!!!

--- LoadTheData.js
+++ LoadTheData.js
@@ -1,15 +1,74 @@
-parseTheJson();
+window.onload = function() {
+  var startPos;
+  var geoOptions = {
+    enableHighAccuracy: true
+  }
+
+  var geoSuccess = function(position) {
+    startPos = position;
+    // document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+    // document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+    console.log("current location "+ startPos.coords.latitude+ startPos.coords.longitude)
+    var marker = new google.maps.Marker({
+      position: {lat: startPos.coords.latitude, lng: startPos.coords.longitude},
+      map: map,
+      title: 'Current Location'
+    });
+  };
+  var geoError = function(error) {
+    console.log('Error occurred. Error code: ' + error.code);
+    // error.code can be:
+    //   0: unknown error
+    //   1: permission denied
+    //   2: position unavailable (error response from location provider)
+    //   3: timed out
+  };
+  var mapLocale = function(position) {
+    startPos = position;
+    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+    console.log("current location "+ startPos.coords.latitude+ startPos.coords.longitude)
+    var marker = new google.maps.Marker({
+      position: {lat: startPos.coords.latitude, lng: startPos.coords.longitude},
+      map: map,
+      title: 'Current Location'
+    });
+  };
+  navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
+};
 
 function parseTheJson () {
   $.getJSON("Resources/dataset.json", function(arr) {
     // console.log(arr);
-    var ListLocations = [];
+
     for (var i = 0; i < arr.length; i++) {
       ListLocations.push(new Entry(arr[i]));
     }
-    console.log(ListLocations);
     return ListLocations
+  }).done(function() {
+    console.log( "We done!" );
+    for (var i = 0; i < ListLocations.length; i++) {
+      console.log(i);
+      var location = ListLocations[i];
+      var marker = new google.maps.Marker({
+        position: {lat: parseFloat(location.LATITUDE), lng: parseFloat(location.LONGITUDE)},
+        map: map,
+        title: "marker"+i
+      });
+    }
   });
+}
+
+function setMarkers(map,locations){
+  console.log("hi set marker");
+  console.log(locations.length);
+  for (var i = 0; i < ListLocations.length; i++) {
+    console.log(i);
+    var location = ListLocations[i];
+    var marker = new google.maps.Marker({
+      position: {lat: parseFloat(location.LATITUDE), lng: parseFloat(location.LONGITUDE)},
+    });
+  }
 }
 
 function Entry (obj) {
